28b143cee63fec6c9b1f3e193948ff42ac969367
DanielTS5 <danielt.soriano@gmail.com>
2025-09-17 21:58:42 -0600

    curso4

---
+++ leccion4/src/main/java/com/curso/leccion4/Leccion4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.curso.leccion4;
+
+/**
+ *
+ * @author danie
+ */
+public class Leccion4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
